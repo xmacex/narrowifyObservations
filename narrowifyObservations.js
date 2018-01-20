abb819c1240c3833ca62272838766c13cee15352
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2018-01-20 23:18:57 +0100

    Ready to go, methinks

---
+++ narrowifyObservations.js
@@ -0,0 +1,26 @@
+function toggleheader(event) {
+    var header = "category, tag\n";
+    var output = document.getElementById("output");
+    if (event.target.checked) {
+	output.innerText = header + output.innerText;
+	output.classList.add("highlightheader");
+    } else {
+	output.innerText = output.innerText.replace(header, "");
+	output.classList.remove("highlightheader");
+    }
+}
+
+function split() {
+    var data = document.getElementById("datainput").value.split('\n');
+    var output = document.getElementById("output");
+    output.innerText += data.map(splitLine).join('');
+}
+
+function splitLine(line) {
+    if (line.includes(',')) {
+	var [head, tail] = line.split(',');
+	return tail.split(';').map(
+	    sf => head.trim() + ', ' + sf.trim() + '\n'
+	).join('');
+    }
+}
